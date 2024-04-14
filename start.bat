f8c94f73adf3d863588f3f661873e64986c335e3
DevTae <dev.taehyeon.kim@gmail.com>
2024-04-15 02:56:23 +0900

    Add docker-compose.yml and orchestration batch file

---
+++ start.bat
@@ -0,0 +1,27 @@
+@echo off
+
+git clone https://github.com/DB-Inc-Capstone/back-eurekaserver.git
+git clone https://github.com/DB-Inc-Capstone/back-apigateway.git
+git clone https://github.com/DB-Inc-Capstone/back-worker.git
+
+
+cd back-eurekaserver && git pull origin main
+cd ..
+
+cd back-apigateway && git pull origin main
+cd ..
+
+cd back-worker && git pull origin main
+cd ..
+
+
+cd back-eurekaserver && .\gradlew.bat build
+cd .. 
+
+cd back-apigateway && .\gradlew.bat build
+cd ..
+
+cd back-worker && .\gradlew.bat build
+cd ..
+
+docker-compose up
